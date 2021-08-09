c1b79335f5565cae819e2ce6a6d789d7bb178f69
Paulo Lima <pk.katon@gmail.com>
2021-08-09 13:55:52 -0300

    feature/inicializacao

---
+++ projects/sol-lib/src/lib/sol-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { SolLibComponent } from './sol-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    SolLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SolLibComponent
+  ]
+})
+export class SolLibModule { }
---
+++ projects/sol-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of sol-lib
+ */
+
+export * from './lib/sol-lib.service';
+export * from './lib/sol-lib.component';
+export * from './lib/sol-lib.module';
