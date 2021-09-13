8b9b1d95d95011915f70464ddbb0145e0b265b59
Paulo Lima <pk.katon@gmail.com>
2021-09-13 13:39:04 -0300

    fix/button-sol-component
    - Inclusões de módulos faltantes
    - Modificações de componentes
    - Inclusões de dependências no package.json

---
+++ projects/sol-components-angular/src/lib/basic-button/basic-button.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasicButtonComponent } from './basic-button.component';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [
+    BasicButtonComponent
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports: [
+    BasicButtonComponent
+  ]
+})
+export class BasicButtonModule { }
