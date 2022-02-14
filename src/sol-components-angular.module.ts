78598215f84541965fcd998f025f5412c3121fdb
Edgard H <edgardhwp@outlook.com>
2022-02-14 11:37:55 -0300

    Correção de grid

--- src/sol-components-angular.module.ts
+++ src/sol-components-angular.module.ts
@@ -1,27 +1,21 @@
-import { NgModule,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { GridExtendedComponent } from './form/elements/grid-extended/grid-extended.component';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+    MatAutocompleteModule, MatBadgeModule, MatExpansionModule, MatGridListModule, MatIconModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSidenavModule, MatSlideToggleModule, MatSnackBarModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule
+} from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import {
-    MatAutocompleteModule,
-    MatRadioModule, MatIconModule,
-    MatSnackBarModule, MatGridListModule,
-    MatMenuModule, MatToolbarModule,
-    MatSidenavModule, MatListModule,
-    MatTooltipModule, MatTableModule,
-    MatSlideToggleModule, MatProgressBarModule, MatExpansionModule, MatBadgeModule, MatTabsModule, MatProgressSpinnerModule, MatTreeModule, MatRippleModule, MatPaginatorModule, MatNativeDateModule
-} from '@angular/material';
+import { MatStepperModule } from '@angular/material/stepper';
+import { GridExtendedComponent } from './form/elements/grid-extended/grid-extended.component';
 
 let components = [
     GridExtendedComponent
